1a9ac28adf4d9c41ad026a1342390f2209e7c67a
Sherydan <tobar.luis.c@gmail.com>
2022-10-17 03:19:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PizzasGallery from "./Pages/PizzasGallery";
+import PizzaDetails from "./Pages/PizzaDetails";
+import { PizzaProvider } from "./context/PizzasContext";
+import { CartProvider } from "./context/CartContext";
+import PizzaCart from "./Pages/PizzaCart";
+import PageNotFound from "./Pages/PageNotFound";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PizzaProvider>
+                <CartProvider>
+                    <Router>
+                        <Navbar />
+                        <Header />
+                        <Routes>
+                            <Route path="*" element={<PageNotFound />} />
+                            <Route path="/" element={<PizzasGallery />} />
+                            <Route
+                                path="/pizza/:pizzaId"
+                                element={<PizzaDetails />}
+                            />
+                            <Route path="/cart" element={<PizzaCart />} />
+                        </Routes>
+                        <Footer />
+                    </Router>
+                </CartProvider>
+            </PizzaProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '1rem',
+      marginTop: '1rem',
+      backgroundColor: 'rgb(23,162,184)',
+    }}>
+      
+      <p style={{
+        marginRight: '1rem',
+      }}>Luis Tobar </p> <span> 2022</span>
+  
+    </footer>
+  )
+}
+
+export default Footer
